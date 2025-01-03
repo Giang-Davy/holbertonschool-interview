58a0b0757fa4499e4b29c0b68ba6e3e2776aeaeb
Giang-Davy <davy.dangvu@gmail.com>
2025-01-03 10:30:24 +0000

    0

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""fonction"""
+
+
+def canUnlockAll(boxes):
+    """Determine if all boxes can be opened."""
+    opened = [0]
+    keys = set(boxes[0])
+    while keys:
+        new_keys = list(keys)
+        keys.clear()
+        for key in new_keys:
+            if key not in opened and key < len(boxes):
+                opened.append(key)
+                keys.update(boxes[key])
+    return len(opened) == len(boxes)
