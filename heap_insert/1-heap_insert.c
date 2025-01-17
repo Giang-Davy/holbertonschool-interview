bb086fea76b0051a3dddb2daa66a710a978c546d
Giang-Davy <davy.dangvu@gmail.com>
2025-01-17 15:14:36 +0000

    1V3

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -10,18 +10,18 @@
  */
 static heap_t *heapify_up(heap_t *node)
 {
-    heap_t *current = node;
-    int temp;
+	heap_t *current = node;
+	int temp;
 
-    while (current->parent && current->n > current->parent->n)
-    {
-        temp = current->n;
-        current->n = current->parent->n;
-        current->parent->n = temp;
-        current = current->parent;
-    }
+	while (current->parent && current->n > current->parent->n)
+	{
+		temp = current->n;
+		current->n = current->parent->n;
+		current->parent->n = temp;
+		current = current->parent;
+	}
 
-    return (current);
+	return (current);
 }
 
 /**
@@ -33,29 +33,29 @@ static heap_t *heapify_up(heap_t *node)
  */
 static heap_t *find_insert_position(heap_t *root)
 {
-    heap_t **queue;
-    heap_t *current;
-    size_t head = 0, tail = 0;
+	heap_t **queue;
+	heap_t *current;
+	size_t head = 0, tail = 0;
 
-    queue = malloc(1024 * sizeof(heap_t *));
-    if (!queue)
-        return (NULL);
+	queue = malloc(1024 * sizeof(heap_t *));
+	if (!queue)
+		return (NULL);
 
-    queue[tail++] = root;
-    while (head < tail)
-    {
-        current = queue[head++];
-        if (!current->left || !current->right)
-        {
-            free(queue);
-            return (current);
-        }
-        queue[tail++] = current->left;
-        queue[tail++] = current->right;
-    }
+	queue[tail++] = root;
+	while (head < tail)
+	{
+		current = queue[head++];
+		if (!current->left || !current->right)
+		{
+			free(queue);
+			return (current);
+		}
+		queue[tail++] = current->left;
+		queue[tail++] = current->right;
+	}
 
-    free(queue);
-    return (NULL);
+	free(queue);
+	return (NULL);
 }
 
 /**
@@ -68,29 +68,29 @@ static heap_t *find_insert_position(heap_t *root)
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *new_node, *parent;
+	heap_t *new_node, *parent;
 
-    if (!root)
-        return (NULL);
+	if (!root)
+		return (NULL);
 
-    if (!*root)
-    {
-        *root = binary_tree_node(NULL, value);
-        return (*root);
-    }
+	if (!*root)
+	{
+		*root = binary_tree_node(NULL, value);
+		return (*root);
+	}
 
-    parent = find_insert_position(*root);
-    if (!parent)
-        return (NULL);
+	parent = find_insert_position(*root);
+	if (!parent)
+		return (NULL);
 
-    new_node = binary_tree_node(parent, value);
-    if (!new_node)
-        return (NULL);
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
 
-    if (!parent->left)
-        parent->left = new_node;
-    else
-        parent->right = new_node;
+	if (!parent->left)
+		parent->left = new_node;
+	else
+		parent->right = new_node;
 
-    return (heapify_up(new_node));
+	return (heapify_up(new_node));
 }
