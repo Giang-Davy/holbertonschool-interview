3221cca5f45f2fc671ed74d3ea62024decaa0659
Giang-Davy <davy.dangvu@gmail.com>
2025-01-24 11:39:09 +0000

    0V2

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -2,19 +2,21 @@
 """fonction"""
 
 
+import math
+
+
 def minOperations(n):
     """min operations to achieve n H's"""
     if n == 1:
         return 0
 
     operations = 0
-
-    for i in range(2, n + 1):
+    for i in range(2, int(math.sqrt(n)) + 1):
         while n % i == 0:
             n //= i
             operations += i
 
+
     if n > 1:
         operations += n
-
     return operations
