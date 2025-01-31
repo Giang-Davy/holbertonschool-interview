feae7689575d20896d1689991e338a1040450378
Giang-Davy <davy.dangvu@gmail.com>
2025-01-31 14:43:58 +0000

    4v5

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -5,13 +5,12 @@
 
 #define SIZE 3
 
-// Déclaration de la fonction pour vérifier s'il y a des cellules instables
-bool there_are_unstable_cells(int grid[SIZE][SIZE]);
+bool there_are_unstable_cells(
+int grid[SIZE][SIZE]);
 
-// Déclaration de la fonction pour effectuer un renversement des grains de sable
 void topple(int grid[SIZE][SIZE]);
 
-// Déclaration de la fonction pour additionner les grilles et effectuer les renversements
-void sandpiles_sum(int grid1[SIZE][SIZE], int grid2[SIZE][SIZE]);
+void sandpiles_sum(int grid1[SIZE][SIZE], 
+int grid2[SIZE][SIZE]);
 
 #endif // SANDPILES_H
