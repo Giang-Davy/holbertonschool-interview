601d4dc943fd50d88f227b88bad2cb110fcf68f4
Giang-Davy <davy.dangvu@gmail.com>
2025-03-07 11:18:42 +0000

    0

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,10 @@
+def validUTF8(data):
+    """verifier une liste en utf"""
+    if not all(0 <= num <= 255 for num in data):
+        return False
+
+    try:
+        bytes(data).decode('utf-8')
+        return True
+    except UnicodeDecodeError:
+        return False
