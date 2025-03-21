43d1c48f7add690242a19a9e071fdb989754b3a5
Giang-Davy <davy.dangvu@gmail.com>
2025-03-21 12:16:34 +0000

    arranger le betty

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -11,29 +11,27 @@
  */
 void menger(int level)
 {
-    if (level < 0)
-        return;
-
-    int size = pow(3, level);
-    for (int row = 0; row < size; row++)
-    {
-        for (int col = 0; col < size; col++)
-        {
-            int r = row, c = col;
-            char isSpace = '#';
-
-            while (r > 0 || c > 0)
-            {
-                if (r % 3 == 1 && c % 3 == 1)
-                {
-                    isSpace = ' ';
-                    break;
-                }
-                r /= 3;
-                c /= 3;
-            }
-            putchar(isSpace);
-        }
-        putchar('\n');
-    }
+	if (level < 0)
+		return;
+	int size = pow(3, level);
+	for (int row = 0; row < size; row++)
+	{
+		for (int col = 0; col < size; col++)
+		{
+			int r = row, c = col;
+			char isSpace = '#';
+			while (r > 0 || c > 0)
+			{
+				if (r % 3 == 1 && c % 3 == 1)
+				{
+					isSpace = ' ';
+					break;
+				}
+				r /= 3;
+				c /= 3;
+			}
+			putchar(isSpace);
+		}
+		putchar('\n');
+	}
 }
