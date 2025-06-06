ac8039afceb24f3c5f8f311944a003fe62eddd7f
Giang-Davy <davy.dangvu@gmail.com>
2025-06-06 14:29:56 +0000

    mise a jour des fichiers

---
+++ find_the_loop/0-find_loop.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * find_listint_loop - détecte le début d'une boucle dans une liste chaînée
+ * @head: pointeur vers le début de la liste
+ *
+ * Return: adresse du nœud où la boucle commence, ou NULL s'il n'y a pas de boucle
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head, *fast = head;
+
+	if (!head || !head->next)
+		return (NULL);
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+	return (NULL);
+}
