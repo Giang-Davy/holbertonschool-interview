15418284b4a3008830bbc767b3d62d5c37c79882
Giang-Davy <davy.dangvu@gmail.com>
2025-06-27 13:16:45 +0000

    ajout du readme et du fichier

---
+++ making_change/0-makin_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""0-making_change.py"""
+
+
+def makeChange(coins, total):
+    """reste"""
+    min_coins = [float('inf')] * (total + 1)
+    min_coins[0] = 0
+
+    for m in range(1, total + 1):
+        best = float('inf')
+        for c in coins:
+            if c <= m and min_coins[m - c] != float('inf'):
+                if min_coins[m - c] + 1 < best:
+                    best = min_coins[m - c] + 1
+        min_coins[m] = best
+
+    if min_coins[total] == float('inf'):
+        return -1
+    return min_coins[total]
