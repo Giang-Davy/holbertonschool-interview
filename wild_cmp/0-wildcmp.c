06c2643c808f0982777a695584cda8c49a37ef72
Giang-Davy <davy.dangvu@gmail.com>
2025-08-01 11:42:19 +0000

    creation des fichiers avec leurs codes

---
+++ wild_cmp/0-wildcmp.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * wildcmp - Compare deux chaînes de caractères, où la deuxième
+ *           peut contenir le caractère spécial '*' qui remplace
+ *           n'importe quelle chaîne (y compris vide).
+ * @s1: Première chaîne de caractères à comparer.
+ * @s2: Deuxième chaîne de caractères à comparer (peut contenir '*').
+ *
+ * Return: 1 si les chaînes peuvent être considérées identiques, 0 sinon.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+
+	if (*s2 == '*')
+	{
+		while (*s2 == '*')
+			s2++;
+		if (*s2 == '\0')
+			return (1);
+		while (*s1 != '\0')
+		{
+			if (wildcmp(s1, s2))
+				return (1);
+			s1++;
+		}
+		return (0);
+	}
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
+}
