a8d4d67b90ea80d8b46e66f7d93ef18686b1cec2
Giang-Davy <davy.dangvu@gmail.com>
2025-08-25 15:34:48 +0000

    ajout du fichier readme et du code

---
+++ primegame/0-prime_game.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""prime_game.py"""
+
+
+def isWinner(x, nums):
+    max_n = max(nums)
+    premier = [True] * (max_n + 1)
+    premier[0] = False
+    premier[1] = False
+
+    i = 2
+    while i * i <= max_n:
+        if premier[i]:
+            j = i * i
+            while j <= max_n:
+                premier[j] = False
+                j += i
+        i += 1
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        count_primes = 0
+        for k in range(2, n + 1):
+            if premier[k]:
+                count_primes += 1
+        if count_primes % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
